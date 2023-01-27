8e272f25576c19074bd066fdf36c4557486327c4
usamaadil16 <104248079+usamaadil16@users.noreply.github.com>
2023-01-27 22:53:51 +0500

    login leads to product page

--- source/repos/Furniture/Furniture/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ source/repos/Furniture/Furniture/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -104,7 +104,7 @@ namespace Furniture.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Product");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
