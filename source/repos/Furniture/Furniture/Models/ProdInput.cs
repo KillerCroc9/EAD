0254083aff853da08d7e50fb19320e98c2de9566
usamaadil16 <104248079+usamaadil16@users.noreply.github.com>
2023-03-03 00:42:32 +0500

    final submission

--- source/repos/Furniture/Furniture/Models/ProdInput.cs
+++ source/repos/Furniture/Furniture/Models/ProdInput.cs
@@ -1,12 +1,18 @@
-﻿namespace Furniture.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Furniture.Models
 {
     public class ProdInput
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public decimal Price { get; set; } = 0;
 
         public IFormFile? file { get; set; }
+        [Required]
+        public long? contact { get; set; }
+
     }
 }
